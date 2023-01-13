8f93d72990dd50067bbda92498d74590a83b04f2
solbae <sol4854@naver.com>
2023-01-13 14:33:08 +0900

    first Commit

---
+++ src/main/java/teamproject/pocoapoco/PocoapocoApplication.java
@@ -0,0 +1,13 @@
+package teamproject.pocoapoco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocoapocoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PocoapocoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/teamproject/pocoapoco/PocoapocoApplicationTests.java
@@ -0,0 +1,13 @@
+package teamproject.pocoapoco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocoapocoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
