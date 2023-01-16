2bf1409d83ce605b0c6ed5f7f8938fbd70daa2b4
solbae <sol4854@naver.com>
2023-01-16 17:27:33 +0900

    fix : User테이블 userName 추가

--- src/main/java/teamproject/pocoapoco/domain/entity/User.java
+++ src/main/java/teamproject/pocoapoco/domain/entity/User.java
@@ -1,88 +1,30 @@
 package teamproject.pocoapoco.domain.entity;
 
-import lombok.*;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
+import lombok.Getter;
 import teamproject.pocoapoco.enums.InterestSport;
 import teamproject.pocoapoco.enums.UserRole;
 
 import javax.persistence.*;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 @Entity
 @Getter
 @Table(name = "Users")
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-public class User implements UserDetails {
+public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String userId;
     private String userName;
     private String password;
     private String address;
+    private InterestSport sport;
     private Integer manner;
-    private UserRole role = UserRole.ROLE_USER;
-
-    @OneToOne
-    private Sport sport;
-    private String profilePath;
+    private UserRole role;
 
     @OneToMany(mappedBy = "user")
     private List<Crew> crews = new ArrayList<>();
 
     @OneToMany(mappedBy = "user")
     private List<Like> likes = new ArrayList<>();
-
-    @OneToMany(mappedBy = "user")
-    private List<Alarm> alarms = new ArrayList<>();
-
-    @OneToMany(mappedBy = "user")
-    private List<Comment> comments = new ArrayList<>();
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return new ArrayList<GrantedAuthority>(List.of(new SimpleGrantedAuthority(role.name())));
-    }
-
-    @Override
-    public String getUsername() {
-        return userName;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return false;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return false;
-    }
-
-    public static User toEntity(String userId, String userName, String address, String password, boolean likeSoccer, boolean likeJogging, boolean likeTennis){
-        return User.builder()
-                .userId(userId)
-                .userName(userName)
-                .address(address)
-                .sport(Sport.setSport(likeSoccer, likeJogging, likeTennis))
-                .password(password)
-                .build();
-    }
-
 }
