beb0d41b74449b2913c234f89ec97a2e4f7e87e7
solbae <sol4854@naver.com>
2023-01-16 16:23:16 +0900

    fix : Crew 테이블 예약어 문제 해결
    Crew 필드의 필드명 예약어 에러로 변경 limit -> crew_limit

--- src/main/java/teamproject/pocoapoco/domain/entity/Like.java
+++ src/main/java/teamproject/pocoapoco/domain/entity/Like.java
@@ -1,16 +1,17 @@
 package teamproject.pocoapoco.domain.entity;
 
+import lombok.Getter;
 import teamproject.pocoapoco.enums.AlarmType;
 
 import javax.persistence.*;
 
 @Entity
+@Table(name = "likes")
 public class Like {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private AlarmType alarm;
-
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
