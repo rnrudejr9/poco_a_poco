e78de5622739ad7850c91a79402b8fe83c4901ef
pi koo <rnrudejr1234@naver.com>
2023-01-16 23:34:13 +0900

    Spring Security 설정

--- src/main/java/teamproject/pocoapoco/domain/entity/User.java
+++ src/main/java/teamproject/pocoapoco/domain/entity/User.java
@@ -5,32 +5,70 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.lang.UsesSunHttpServer;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import teamproject.pocoapoco.enums.InterestSport;
 import teamproject.pocoapoco.enums.UserRole;
 
 import javax.persistence.*;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Entity
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-public class User {
+@Builder
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String userName;
     private String password;
     private String address;
     private InterestSport sport;
     private Integer manner;
     private UserRole role;
 
-    @OneToMany(mappedBy = "user")
-    @Builder.Default
-    private List<Crew> crews = new ArrayList<>();
+//    @OneToMany(mappedBy = "user")
+//    @Builder.Default
+//    private List<Crew> crews = new ArrayList<>();
+//
+//    @OneToMany(mappedBy = "user")
+//    @Builder.Default
+//    private List<Like> likes = new ArrayList<>();
 
-    @OneToMany(mappedBy = "user")
-    @Builder.Default
-    private List<Like> likes = new ArrayList<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return new ArrayList<GrantedAuthority>(List.of(new SimpleGrantedAuthority(role.name())));
+    }
+
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
