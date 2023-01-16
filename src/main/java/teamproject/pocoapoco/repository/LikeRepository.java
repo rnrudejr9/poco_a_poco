34913191b0551e245f8fde59829a102f6adfa47a
solbae <sol4854@naver.com>
2023-01-16 16:23:16 +0900

    fix : Crew 테이블 예약어 문제 해결
    Crew 필드의 필드명 예약어 에러로 변경 limit -> crew_limit

--- src/main/java/teamproject/pocoapoco/repository/LikeRepository.java
+++ src/main/java/teamproject/pocoapoco/repository/LikeRepository.java
@@ -1,8 +1,8 @@
-package teamproject.pocoapoco.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import teamproject.pocoapoco.domain.entity.Like;
-import teamproject.pocoapoco.domain.entity.User;
-
-public interface LikeRepository extends JpaRepository<Like, Long> {
-}
+//package teamproject.pocoapoco.repository;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import teamproject.pocoapoco.domain.entity.Like;
+//import teamproject.pocoapoco.domain.entity.User;
+//
+//public interface LikeRepository extends JpaRepository<Like, Long> {
+//}
