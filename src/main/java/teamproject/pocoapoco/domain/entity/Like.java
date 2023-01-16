8fd4af2a8a3a4056b3d8a4446c1e858e182b9e9f
solbae <sol4854@naver.com>
2023-01-16 14:22:09 +0900

    feat : Like 필드변경
    Like시 알려주는 AlarmType의 필드명 변경 massage -> alarm

--- src/main/java/teamproject/pocoapoco/domain/entity/Like.java
+++ src/main/java/teamproject/pocoapoco/domain/entity/Like.java
@@ -9,13 +9,13 @@ public class Like {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private AlarmType massage;
+    private AlarmType alarm;
 
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
-
+\
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "crew_id")
     private Crew crew;
