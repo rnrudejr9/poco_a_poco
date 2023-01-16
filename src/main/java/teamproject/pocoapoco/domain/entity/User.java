876b14e29d5a8e59e61be27e3ab399fe7b4a880a
solbae <sol4854@naver.com>
2023-01-16 17:27:33 +0900

    fix : User테이블 userName 추가

--- src/main/java/teamproject/pocoapoco/domain/entity/User.java
+++ src/main/java/teamproject/pocoapoco/domain/entity/User.java
@@ -15,6 +15,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String userName;
     private String password;
     private String address;
     private InterestSport sport;
