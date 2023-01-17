24e779b6eeed25f7f985f0a47084065372b7065c
sanghee <wjdtkdgml7352@daum.net>
2023-01-17 13:07:58 +0900

    feat : security 관련 ErrorCode 추가 - FORBIDDEN_REQUEST : 접근 불가 - EXPIRED_TOKEN : 토큰 만료

--- src/main/java/teamproject/pocoapoco/exception/ErrorCode.java
+++ src/main/java/teamproject/pocoapoco/exception/ErrorCode.java
@@ -20,12 +20,18 @@ public enum ErrorCode {
     USERID_NOT_FOUND(HttpStatus.NOT_FOUND, "아이디가 존재하지 않습니다."),
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "패스워드가 잘못되었습니다."),
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "잘못된 토큰입니다."),
+    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 토큰입니다."),
 
     /**
      *  모임관련 로직 예외
      */
     CREW_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 모임이 없습니다."),
     LIMIT_NUMBER_EXCEED(HttpStatus.CONFLICT,"모임 인원수를 초과하였습니다."),
+
+    /**
+     * 접근 권한 예외
+     */
+    FORBIDDEN_REQUEST(HttpStatus.FORBIDDEN, "ADMIN 회원만 접근할 수 있습니다."),
 
     /**
      * 그 외
