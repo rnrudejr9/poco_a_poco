a869b2607e11490e8b0435c8211b1f6996b358a8
solbae <sol4854@naver.com>
2023-01-17 17:25:55 +0900

    feat : 댓글 알림 엔티티 작성

---
+++ src/main/java/teamproject/pocoapoco/domain/entity/Comment.java
@@ -0,0 +1,27 @@
+package teamproject.pocoapoco.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String comment;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "crew_id")
+    Crew crew;
+}
