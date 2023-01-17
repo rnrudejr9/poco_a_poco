1cb73b9eee424809d87803bf988fddc070a0e173
KimDohyoun79 <dogdo5726@gmail.com>
2023-01-17 13:39:18 +0900

    feat : 로그인 기능 수정

---
+++ src/main/java/teamproject/pocoapoco/enums/login/logindto/Response.java
@@ -0,0 +1,17 @@
+package teamproject.pocoapoco.enums.login.logindto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Response<T> {
+    private String resultCode;
+    private T result;
+
+    public static <T> Response<T> success(T result) {
+        return new Response("SUCCESS", result);
+    }
+
+}
