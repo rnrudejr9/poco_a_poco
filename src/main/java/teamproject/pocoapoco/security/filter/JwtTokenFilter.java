5d98f8d17312f709c121536f1706163c874ff3d2
sanghee <wjdtkdgml7352@daum.net>
2023-01-17 13:09:14 +0900

    feat : Error response 정적메소드 작성 - MakeError() : Security Chain 에서 발생하는 에러 응답

--- src/main/java/teamproject/pocoapoco/security/filter/JwtTokenFilter.java
+++ src/main/java/teamproject/pocoapoco/security/filter/JwtTokenFilter.java
@@ -1,10 +1,14 @@
 package teamproject.pocoapoco.security.filter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
+import teamproject.pocoapoco.domain.dto.error.ErrorResponse;
+import teamproject.pocoapoco.domain.dto.response.Response;
+import teamproject.pocoapoco.exception.ErrorCode;
 import teamproject.pocoapoco.security.provider.JwtProvider;
 
 import javax.servlet.FilterChain;
@@ -15,6 +19,10 @@ import java.io.IOException;
 
 @RequiredArgsConstructor
 public class JwtTokenFilter extends OncePerRequestFilter {
+    /**
+     * request 에서 전달받은 Jwt 토큰을 확인
+     */
+
     private final String BEARER = "Bearer ";
 
     private final JwtProvider jwtProvider;
@@ -46,5 +54,21 @@ public class JwtTokenFilter extends OncePerRequestFilter {
 
     private String parseBearer(String token) {
         return token.substring(BEARER.length());
+    }
+
+    /**
+     * Security Chain 에서 발생하는 에러 응답 구성
+     */
+    public static void MakeError(HttpServletResponse response , ErrorCode errorCode) throws IOException {
+        response.setContentType("application/json;charset=UTF-8");
+        response.setStatus(errorCode.getHttpStatus().value());
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        ErrorResponse errorResponse = new ErrorResponse
+                (errorCode, errorCode.getMessage());
+        Response<ErrorResponse> resultResponse = Response.error(errorResponse);
+
+        // 한글 출력을 위해 getWriter()
+        response.getWriter().write(objectMapper.writeValueAsString(resultResponse));
     }
 }
