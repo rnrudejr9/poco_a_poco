039003267ca8dd35e25957e074e95f9e09095c41
대환 <eoghks960307@gmail.com>
2023-01-20 11:53:56 +0900

    chore : api호출 형식 통일 (#12)

--- src/main/java/teamproject/pocoapoco/controller/FollowController.java
+++ src/main/java/teamproject/pocoapoco/controller/FollowController.java
@@ -19,13 +19,13 @@ public class FollowController {
     private final FollowService followService;
     @ApiOperation(value = "follow", notes = "해당 유저를 follow 합니다.")
     @PostMapping("/{userId}/follow")
-    public Response<?> follow(@PathVariable String userId, Authentication authentication){
+    public Response<?> follow(@PathVariable Long userId, Authentication authentication){
 
      return Response.success(followService.follow(authentication.getName(),userId));
     }
     @ApiOperation(value = "unfollow", notes = "해당 유저를 unfollow 합니다.")
     @DeleteMapping("/{userId}/unfollow")
-    public Response<?> unFollow(@PathVariable String userId, Authentication authentication){
+    public Response<?> unFollow(@PathVariable Long userId, Authentication authentication){
 
         return Response.success(followService.unFollow(authentication.getName(),userId));
     }
@@ -34,7 +34,8 @@ public class FollowController {
     public Response<?> followedCount(@PathVariable Long userId){
 
         return Response.success(followService.followedCount(userId));
-    }@ApiOperation(value = "팔로잉 수", notes = "해당 유저가 팔로우 하고있는 유저의 수를 표시합니다.")
+    }
+    @ApiOperation(value = "팔로잉 수", notes = "해당 유저가 팔로우 하고있는 유저의 수를 표시합니다.")
     @GetMapping("/{userId}/followingCount")
     public Response<?> followingCount(@PathVariable Long userId){
 
--- src/main/java/teamproject/pocoapoco/service/FollowService.java
+++ src/main/java/teamproject/pocoapoco/service/FollowService.java
@@ -14,14 +14,18 @@ import teamproject.pocoapoco.repository.UserRepository;
 public class FollowService {
     private final UserRepository userRepository;
     private final FollowRepository followRepository;
-    public String follow(String followingUserId, String userId){
+    public String follow(String followingUserId, Long userId){
+
         User followingUser = userRepository.findByUserId(followingUserId).orElseThrow(()->
-        {throw new AppException(ErrorCode.USERID_NOT_FOUND,ErrorCode.USERID_NOT_FOUND.getMessage());
+        {
+            throw new AppException(ErrorCode.USERID_NOT_FOUND,ErrorCode.USERID_NOT_FOUND.getMessage());
         });
 
-        User user = userRepository.findByUserId(userId).orElseThrow(()->
-        {throw new AppException(ErrorCode.USERID_NOT_FOUND,ErrorCode.USERID_NOT_FOUND.getMessage());
+        User user = userRepository.findById(userId).orElseThrow(()->
+        {
+            throw new AppException(ErrorCode.USERID_NOT_FOUND,ErrorCode.USERID_NOT_FOUND.getMessage());
         });
+        //user 와 login한 user의 id가 같은경우
         if(userId.equals(followingUserId)){
             throw new AppException(ErrorCode.WRONG_PATH,"자기 자신을 팔로우 할 수 없습니다.");
         }
@@ -33,35 +37,43 @@ public class FollowService {
         return user.getUsername()+"님을 팔로우 합니다.";
 
     }
-    public String unFollow(String unFollowingUserId, String userId){
+    public String unFollow(String unFollowingUserId, Long userId){
+
         User unFollowingUser = userRepository.findByUserId(unFollowingUserId).orElseThrow(()->
-        {throw new AppException(ErrorCode.USERID_NOT_FOUND,ErrorCode.USERID_NOT_FOUND.getMessage());
+        {
+            throw new AppException(ErrorCode.USERID_NOT_FOUND,ErrorCode.USERID_NOT_FOUND.getMessage());
         });
 
-        User user = userRepository.findByUserId(userId).orElseThrow(()->
-        {throw new AppException(ErrorCode.USERID_NOT_FOUND,ErrorCode.USERID_NOT_FOUND.getMessage());
+        User user = userRepository.findById(userId).orElseThrow(()->
+        {
+            throw new AppException(ErrorCode.USERID_NOT_FOUND,ErrorCode.USERID_NOT_FOUND.getMessage());
         });
+
         if(userId.equals(unFollowingUserId)){
             throw new AppException(ErrorCode.WRONG_PATH,"자기 자신을 팔로우 취소할 수 없습니다.");
         }
 
         Follow follow = followRepository.findByFollowingUserIdAndFollowedUserId(unFollowingUser.getId(),user.getId()).orElseThrow(()->
-        {throw new AppException(ErrorCode.WRONG_PATH,"해당 유저를 팔로우 하고 있지 않습니다");
+        {
+            throw new AppException(ErrorCode.WRONG_PATH,"해당 유저를 팔로우 하고 있지 않습니다");
 
         });
         followRepository.delete(follow);
-
         return user.getUsername()+"님을 팔로우 취소합니다.";
-
 
     }
     public Integer followedCount(Long userId){ //해당 유저를 팔로우 하고 있는 유저의 수
+        User user = userRepository.findById(userId).orElseThrow(()->
+        {
+            throw new AppException(ErrorCode.USERID_NOT_FOUND,ErrorCode.USERID_NOT_FOUND.getMessage());
+        });
         return followRepository.countByFollowedUserId(userId);
     }
-
     public Integer followingCount(Long userId){ //해당 유자가 팔로잉 하고 있는 유저의 수
+        User user = userRepository.findById(userId).orElseThrow(()->
+        {
+            throw new AppException(ErrorCode.USERID_NOT_FOUND,ErrorCode.USERID_NOT_FOUND.getMessage());
+        });
         return followRepository.countByFollowingUserId(userId);
     }
-
-
 }
