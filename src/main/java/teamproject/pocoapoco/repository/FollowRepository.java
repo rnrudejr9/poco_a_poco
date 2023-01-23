bdeedc130a327bf9376ae0eb3aa2e45af9f97cb7
대환 <eoghks960307@gmail.com>
2023-01-23 23:16:27 +0900

    Refactor : 팔로잉, 팔로워리스트 추가 (#12)

--- src/main/java/teamproject/pocoapoco/repository/FollowRepository.java
+++ src/main/java/teamproject/pocoapoco/repository/FollowRepository.java
@@ -1,5 +1,7 @@
 package teamproject.pocoapoco.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import teamproject.pocoapoco.domain.entity.Follow;
 import teamproject.pocoapoco.domain.entity.User;
@@ -12,4 +14,7 @@ public interface FollowRepository extends JpaRepository<Follow,Long> {
 
     Integer countByFollowingUserId(Long followingUserId);
     Integer countByFollowedUserId(Long followedUserId);
+
+    Page<Follow> findByFollowingUserId(Pageable pageable,Long followingUserId);
+    Page<Follow> findByFollowedUserId(Pageable pageable,Long followedUserId);
 }
