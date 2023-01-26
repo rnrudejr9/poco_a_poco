c9d0d9af1d2de2f453171d764e55759129961e78
대환 <eoghks960307@gmail.com>
2023-01-26 13:26:12 +0900

    Feat : Login page ,signup page    (#17)

--- src/main/java/teamproject/pocoapoco/controller/ViewController.java
+++ src/main/java/teamproject/pocoapoco/controller/ViewController.java
@@ -1,16 +1,44 @@
 package teamproject.pocoapoco.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import teamproject.pocoapoco.domain.dto.user.UserJoinRequest;
+import teamproject.pocoapoco.domain.dto.user.UserLoginRequest;
+import teamproject.pocoapoco.service.UserService;
 
 @Controller
 @Slf4j
+@RequiredArgsConstructor
 public class ViewController {
 
-    @RequestMapping("/")
-    public String test() {
+    private final UserService userService;
+    @GetMapping("/login")
+    public String loginForm(Model model) {
+        model.addAttribute("userLoginRequest",new UserLoginRequest());
 
-        return "sign/sign";
+        return "sign/login";
+    }
+    @GetMapping("/signup")
+    public String signupForm(Model model) {
+        model.addAttribute("userJoinRequest",new UserJoinRequest());
+
+        return "sign/signup";
+    }
+
+    @PostMapping("/signup")
+    public String signup(UserJoinRequest userJoinRequest){
+
+        userService.saveUser(userJoinRequest);
+        return "sign/main";
+    }
+    @PostMapping("/signin")
+    public String login(UserLoginRequest userLoginRequest){
+
+        userService.login(userLoginRequest);
+        return "sign/main";
     }
 }
--- src/main/java/teamproject/pocoapoco/domain/dto/user/UserLoginRequest.java
+++ src/main/java/teamproject/pocoapoco/domain/dto/user/UserLoginRequest.java
@@ -7,6 +7,7 @@ import lombok.*;
 @NoArgsConstructor
 @Getter
 @ToString
+@Setter
 public class UserLoginRequest {
     private String userId;
     private String password;
