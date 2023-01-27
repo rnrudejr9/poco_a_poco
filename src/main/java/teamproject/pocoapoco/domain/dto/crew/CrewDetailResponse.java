d9552b6d9713b13768c7d68445ccc004c1bcfdef
sanghee <wjdtkdgml7352@daum.net>
2023-01-27 13:07:30 +0900

    feat : 모임 상세 페이지 view 상세페이지에 모임을 생성한 사용자의 id를 띄우기 위해 crew에 필드 추가 했습니다.

--- src/main/java/teamproject/pocoapoco/domain/dto/crew/CrewDetailResponse.java
+++ src/main/java/teamproject/pocoapoco/domain/dto/crew/CrewDetailResponse.java
@@ -1,10 +1,13 @@
 package teamproject.pocoapoco.domain.dto.crew;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import teamproject.pocoapoco.domain.entity.Crew;
+
+import java.time.LocalDateTime;
 
 @Builder
 @AllArgsConstructor
@@ -16,6 +19,10 @@ public class CrewDetailResponse {
     private String title;
     private String content;
     private Integer crewLimit;
+    private String userId;
+//    private String createdAt;
+//    private String lastModifiedAt;
+//    private String sport;
 
     public static CrewDetailResponse of(Crew crew) {
         return CrewDetailResponse.builder()
@@ -23,6 +30,10 @@ public class CrewDetailResponse {
                 .title(crew.getTitle())
                 .content(crew.getContent())
                 .crewLimit(crew.getCrewLimit())
+                .userId(crew.getUser().getUserId())
+//                .createdAt(crew.getCreatedAt().toString())
+//                .lastModifiedAt(crew.getLastModifiedAt().toString())
+//                .sport(crew.getSport().getName())
                 .build();
     }
 
