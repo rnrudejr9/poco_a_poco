5c1af0ba7a713dc6c1d6ad985821b91fb3a1aba1
Koogling <rnrudejr1234@naver.com>
2023-02-01 18:52:01 +0900

    feat : 채팅 UI 및 서비스 로직 추가
    1. 채팅방 개설 후 입장하여 웹소켓 채팅 시도
    2. (추후 모임이 개설되고 모임채팅방으로 리펙토링

---
+++ src/main/java/teamproject/pocoapoco/config/chat/WebSocketConfig.java
@@ -0,0 +1,26 @@
+package teamproject.pocoapoco.config.chat;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@EnableWebSocketMessageBroker
+@Configuration
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/stomp/chat")
+                .setAllowedOrigins("http://*:*")
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.setApplicationDestinationPrefixes("/pub");
+        config.enableSimpleBroker("/sub");
+    }
+
+}
