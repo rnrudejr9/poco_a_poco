a8fc51d1afe43adfa21c4ab6feb841dcfda85af9
solbae <sol4854@naver.com>
2023-02-02 10:19:22 +0900

    feat : OAuth 로그인

--- src/main/java/teamproject/pocoapoco/repository/UserRepository.java
+++ src/main/java/teamproject/pocoapoco/repository/UserRepository.java
@@ -1,6 +1,8 @@
 package teamproject.pocoapoco.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import teamproject.pocoapoco.domain.entity.User;
 
 import java.util.Optional;
@@ -11,4 +13,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findById(Long userId);
 
     Optional<User> findByUserName(String userName);
+
+    @Query("select m from User m where m.userId=?1")
+    User findOauthUser(String username);
 }
