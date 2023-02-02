6d8f2454f15f38f3c81a36cfb17805a648fb6d3b
solbae <sol4854@naver.com>
2023-02-03 02:33:56 +0900

    feat : 세션정보 redis에 저장

--- src/main/java/teamproject/pocoapoco/PocoapocoApplication.java
+++ src/main/java/teamproject/pocoapoco/PocoapocoApplication.java
@@ -3,9 +3,10 @@ package teamproject.pocoapoco;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
-//@EnableJpaAuditing
+@EnableRedisHttpSession
 public class PocoapocoApplication {
 
     public static void main(String[] args) {
