38bcf62d1995ae8a37622cf3d57991ceadf007cd
solbae <sol4854@naver.com>
2023-02-06 18:01:39 +0900

    feat : 실시간 알림기능

---
+++ src/main/resources/static/js/sse.js
@@ -0,0 +1,20 @@
+
+let userId = document.getElementById("myName").innerText;
+
+    console.log(userId);
+
+    if (userId.length > 0) {
+    const eventSource = new EventSource("/sse" + "?userId=" + userId)
+    eventSource.addEventListener("addComment", function (event) {
+        let message = event.data;
+        Swal.fire({
+            // toast:true,
+            position: 'top-end',
+            icon: 'success',
+            title: message,
+            showConfirmButton: false,
+            timer: 1500
+        });
+
+    });
+}
