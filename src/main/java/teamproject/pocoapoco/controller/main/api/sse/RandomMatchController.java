f0460d1a817a843f3b0e6763d97fc29fac3b5c81
solbae <sol4854@naver.com>
2023-02-09 23:06:26 +0900

    feat : random Match Sse 설정 중

--- src/main/java/teamproject/pocoapoco/controller/main/api/sse/RandomMatchController.java
+++ src/main/java/teamproject/pocoapoco/controller/main/api/sse/RandomMatchController.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import static teamproject.pocoapoco.controller.main.api.sse.SseController.isRandomMatchChecker;
+import static teamproject.pocoapoco.controller.main.api.sse.SseController.sseEmitters;
 
 @RestController
 @RequestMapping("/api/v1/match")
@@ -16,15 +20,27 @@ public class RandomMatchController {
 
     static int randomMatchCnt;
     static int matchOrder;
+    static int cnt;
+
 
     private final RedisTemplate<String, String> redisTemplate;
     @PostMapping("/random")
     public int randomMatch(@RequestParam String username) {
         if(randomMatchCnt % 3 == 0) randomMatchCnt = 0;
-        log.info("username = {}", username);
+        log.info("RandomMatchController - username = {}", username);
         redisTemplate.opsForZSet().add("randomMatching", username, ++matchOrder);
 //        String isInit = redisTemplate.opsForZSet().;
 //        log.info("redis에 저장되었는지 확인 = {}",isInit);
+
+        isRandomMatchChecker = true;
+        SseEmitter emitter = sseEmitters.get("random");
+        try {
+            emitter.send(SseEmitter.event().name("random").data(
+                    ++cnt));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         return ++randomMatchCnt;
     }
 
--- src/main/resources/static/js/random.js
+++ src/main/resources/static/js/random.js
@@ -2,25 +2,23 @@ function randomMatch() {
 
     let username = document.getElementById("myName").innerText;
     console.log(username);
+
+
+
     $.ajax({
         type: "POST",
         url: '/api/v1/match/random'+ "?username=" + username,
         success: function (data) {
             let listCnt = data;
-            if (listCnt != 0) {
+            if (listCnt > 0) {
                 Swal.fire({
                     icon: 'success',
                     title: '랜덤매칭이 시작되었습니다.\n' +
                         '매칭이 될때까지 대기해 주세요👍\n' +
                         '3명이 대기열에 들어오면 매칭됩니다',
                 });
-                $("#randomCnt").empty();
-                $("#randomCnt").append('현재 대기중인 인원 :' + listCnt);
-            } else if (listCnt == 0) {
-                alert("좋아요를 취소하시겠습니까?")
-                // $("#likeCnt").empty();
-                // $("#likeCnt").append(data.count);
-
+                // $("#randomCnt").empty();
+                // $("#randomCnt").append('현재 대기중인 인원 :' + listCnt);
             }
         },
         error: function (request, status, error) {
@@ -28,4 +26,23 @@ function randomMatch() {
             // alert(request.responseText);
         }
     });
+
+    // if (username.length > 0) {
+    //     const randomEventSource = new EventSource("/sse/random")
+    //     randomEventSource.onmessage(event => {
+    //         const p = document.createElement("p")
+    //         p.innerText = event.data;
+    //         console.log(p);
+    //         document.getElementById("messages").appendChild(p);
+    //
+    //
+    //         let listCnt = event.data;
+    //         console.log('데이터 = ' + listCnt);
+    //         $("randomCnt").empty();
+    //         $("randomCnt").append('현재 대기중인 인원 :' + listCnt);
+    //     });
+    // }
+
+
+
 }
