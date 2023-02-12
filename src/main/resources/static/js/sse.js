96acb1b96df2bae496966f9d7a029f7b15af5755
solbae <sol4854@naver.com>
2023-02-12 22:38:57 +0900

    fix : 컨테이너 경로 수정

--- src/main/resources/static/js/sse.js
+++ src/main/resources/static/js/sse.js
@@ -28,7 +28,8 @@ if (userId.length > 0) {
         })
             .then(function (){
                 //date 채팅ID " " 크루ID
-                location.href="http://localhost:8080/view/v1/room?roomId="+split[0]+"&crewId=" + split[1];
+                // http://ec2-52-79-236-51.ap-northeast-2.compute.amazonaws.com:8080/view/v1/start
+                location.href="http://ec2-52-79-236-51.ap-northeast-2.compute.amazonaws.com:8080/view/v1/room?roomId="+split[0]+"&crewId=" + split[1];
             });
 
     });
